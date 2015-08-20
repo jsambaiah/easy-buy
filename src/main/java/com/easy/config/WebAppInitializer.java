32e3fbd76d26860faf91c1d8929bdbb1dc6e34c6
danduphalgunkumar <danduphalgunkumar@gmail.com>
2015-08-20 23:01:00 +0400

    Adding some more config classes

---
+++ src/main/java/com/easy/config/WebAppInitializer.java
@@ -0,0 +1,35 @@
+package com.easy.config;
+
+
+import com.easy.config.root.AppSecurityConfig;
+import com.easy.config.servlet.ServletContextConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ *
+ * Replacement for most of the content of web.xml, sets up the root and the servlet context config.
+ *
+ */
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{AppSecurityConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[] {ServletContextConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+
+
+
+}
+
+
